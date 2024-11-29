4f601575404e05a373e7925f5c652559d16aad4a
T8 <108910965+T888R@users.noreply.github.com>
2024-11-29 10:49:18 -0700

    change player to use groups and added asteroids

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import pygame
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 from constants import *
 from player import Player
 
@@ -10,8 +12,17 @@ def main():
     print("Screen width: %s" % (SCREEN_WIDTH))
     print("Screen height: %s" % (SCREEN_HEIGHT))
 
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+    field = AsteroidField()
     ship = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
 
@@ -21,8 +32,13 @@ def main():
                 return
 
         screen.fill("black")
-        ship.draw(screen)
-        ship.update(dt)
+
+        for update in updatable:
+            update.update(dt)
+
+        for draw in drawable:
+            draw.draw(screen)
+
         pygame.display.flip()
 
         dt = clock.tick(60) / 1000
